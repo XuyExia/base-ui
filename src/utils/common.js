6fcb7dc35da1d2d18c5e8fa17d64444f18fa9644
xuyifan <1135672070@qq.com>
2020-04-02 10:51:57 +0800

    sss

--- src/utils/common.js
+++ src/utils/common.js
@@ -4,18 +4,18 @@ const TokenKey = 'authCode'
 
 // 从sessionStorage中获取token
 const getToken = function getToken () {
-    // return Cookies.get(TokenKey)
-    return localStorage.getItem(TokenKey)
+  // return Cookies.get(TokenKey)
+  return localStorage.getItem(TokenKey)
 }
 
 // 给sessionStorage设置token
 const setToken = function setToken (token) {
-    // return Cookies.set(TokenKey, token)
-    return localStorage.setItem(TokenKey, token)
+  // return Cookies.set(TokenKey, token)
+  return localStorage.setItem(TokenKey, token)
 }
 
 // 从sessionStorage移除token
 const removeToken = function removeToken () {
-    // return Cookies.remove(TokenKey)
-    return localStorage.removeItem(TokenKey)
+  // return Cookies.remove(TokenKey)
+  return localStorage.removeItem(TokenKey)
 }
