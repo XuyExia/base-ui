0216ae79b36b06e3ff91d8ba2ceaf37f97021ebc
xuyifan <1135672070@qq.com>
2020-04-02 10:57:01 +0800

    sss

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,18 @@
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    beforeCreate () {
-    /*  for (const key in sessionStorage) {
-        // eslint-disable-next-line no-prototype-builtins
-        if (sessionStorage.hasOwnProperty(key)) {
-          if (!key.startsWith('yuejuan-')) {
-            continue
-          }
-          let d = sessionStorage.getItem(key)
-          let str = key.replace('yuejuan-', '')
-          try {
-            d = JSON.parse(d)
-            this.$store.commit(str, d)
-            // eslint-disable-next-line no-empty
-          } catch (error) {
-
-          }
-        }
-      }
-      if (localStorage.user) {
-        this.$store.commit('SET_USER', JSON.parse(localStorage.user))
-      }*/
-    }
-  }
+export default {
+  name: 'App'
+}
 </script>
 
-
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
